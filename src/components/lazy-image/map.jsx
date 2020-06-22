8d96db648364681243e64b4c1c84dc2e7e382050
Edmond O'Flynn <oflynned@tcd.ie>
2020-06-22 20:56:45 +0200

    add map with lazy loading

--- src/components/lazy-image/map.jsx
+++ src/components/lazy-image/map.jsx
@@ -13,13 +13,13 @@ import Img from "gatsby-image"
  * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-const Image = () => {
+export const Map = () => {
   const data = useStaticQuery(
     graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "gatsby-icon.png" }) {
+      placeholderImage: file(relativePath: { eq: "map.jpg" }) {
         childImageSharp {
-          fluid(maxWidth: 300) {
+          fluid(maxWidth: 1440) {
             ...GatsbyImageSharpFluid
           }
         }
@@ -29,5 +29,3 @@ const Image = () => {
 
   return <Img fluid={data.placeholderImage.childImageSharp.fluid}/>
 }
-
-export default Image
--- src/components/sections/hero.section.jsx
+++ src/components/sections/hero.section.jsx
@@ -4,6 +4,10 @@ import { Card } from "../card"
 import { EditText } from "../edit-text"
 import { PrimaryButton } from "../button"
 
+import { Map } from "../lazy-image/map"
+
+import "./hero.section.scss"
+
 const expertiseAreas = [
   "Advertising",
   "Content marketing",
@@ -24,34 +28,53 @@ const expertiseAreas = [
   "Copywriting",
   "Mobile app development",
   "Web design"
-].map(exp => exp.toLowerCase())
+]
 
 export const Hero = () => {
   return (
-    <section className={"fill hero"}>
-      <div className={"expertise-titles"}>
-        <div className={"title"}>
-          <h1>Find your future</h1>
-          <div className={"expertise"}>
-            <TextLoop interval={2000} children={expertiseAreas}/>
-          </div>
-          <h1>agency now</h1>
-          <p>Agencies sorted by us. Rated by clients.</p>
-        </div>
-
-        <Card className={"search-card"}>
-          <div>
-            <span className={"material-icons"}>search</span>
-            <EditText placeholder={"What service do you need?"}/>
-          </div>
-          <PrimaryButton>
-            <div className={"find-agency"}>
-              <span>Find my agency</span>
-              <span className={"material-icons"}>arrow_forward</span>
-            </div>
-          </PrimaryButton>
-        </Card>
+    <>
+      <div className={"map"}>
+        <Map/>
       </div>
-    </section>
+      <section className={"fill hero"}>
+
+        <div className={"expertise-titles"}>
+          <div className={"title"}>
+            <h1>Find your future</h1>
+            <div className={"expertise"}>
+              <TextLoop interval={2000} children={expertiseAreas.map(exp => exp.toLowerCase())}/>
+            </div>
+            <h1>agency now</h1>
+            <p>Agencies sorted by us. Rated by clients.</p>
+          </div>
+
+          <Card className={"search-card"}>
+            <div>
+              <span className={"material-icons primary"}>search</span>
+              <EditText placeholder={"What services do you need?"}/>
+            </div>
+            <PrimaryButton>
+              <div className={"find-agency"}>
+                <span>Find my agency</span>
+                <span className={"material-icons"}>arrow_forward</span>
+              </div>
+            </PrimaryButton>
+          </Card>
+
+          <div className={"expertise-links"}>
+            <ul>
+              {expertiseAreas.slice(0, 10)
+                .map((exp, index) => {
+                  if (index < 9) {
+                    return <li key={exp}>{exp}<span>|</span></li>
+                  }
+
+                  return <li key={exp}>{exp}</li>
+                })}
+            </ul>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
