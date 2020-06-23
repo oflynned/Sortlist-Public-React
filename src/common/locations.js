685ef05323058d664fc17eb823be9682eafc839e
Edmond O'Flynn <oflynned@tcd.ie>
2020-06-23 13:10:32 +0200

    add locations and skills static pool

---
+++ src/common/locations.js
@@ -0,0 +1,7 @@
+export const locations = [
+  "Dublin",
+  "Cork",
+  "Galway",
+  "Belfast",
+  "Waterford"
+]
---
+++ src/common/skills.js
@@ -0,0 +1,34 @@
+export const skills = [
+  "Advertising",
+  "Content marketing",
+  "Digital marketing",
+  "Video production",
+  "Design",
+  "Web development",
+  "Social media",
+  "3D",
+  "Online advertising",
+  "Media",
+  "SEO",
+  "Gaming",
+  "Data consulting",
+  "Innovation consulting",
+  "Web analytics",
+  "Public relations",
+  "Copywriting",
+  "Mobile app development",
+  "Web design"
+]
+
+const shuffle = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]
+  }
+
+  return array
+}
+
+export const getRandomSkills = (n = 10) => {
+  return shuffle(skills.slice(0, n))
+}
