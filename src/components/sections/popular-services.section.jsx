80b340bece9d2c6f1b5a4d8f8c6a014a243aaab9
Edmond O'Flynn <oflynned@tcd.ie>
2020-06-23 13:11:09 +0200

    add popular services section

---
+++ src/components/sections/popular-services.section.jsx
@@ -0,0 +1,73 @@
+import React, { Component, useEffect, useState } from "react"
+
+import { SkillCard } from "../skill-card"
+
+import { getRandomSkills, skills } from "../../common/skills"
+import { locations } from "../../common/locations"
+
+import "./popular-services.section.scss"
+
+const skillsPerLocation = locations.map(location => {
+  return {
+    location,
+    skills: getRandomSkills(4)
+  }
+})
+
+const flattenSkillsToLocation = (skillsPerLocation) => {
+  return skillsPerLocation.flatMap(({ location, skills }) => {
+    return skills.map(skill => {
+      return { location, skill }
+    })
+  })
+}
+
+export const PopularServices = () => {
+  const [selectedTabIndex, setSelectedTabIndex] = useState(0)
+  const [filteredSkills, setFilteredSkills] = useState([])
+
+  useEffect(() => {
+    const filterSkills = tabIndex => {
+      const tagLocation = skillsPerLocation[tabIndex].location
+      const skillsForGivenTag = skillsPerLocation.filter(({ location }) => location === tagLocation)
+      return flattenSkillsToLocation(skillsForGivenTag)
+    }
+
+    const skills = filterSkills(selectedTabIndex)
+    setFilteredSkills(skills)
+  }, [selectedTabIndex])
+
+
+  return (
+    <section className={"half popular-services"}>
+      <h1 className={"fancy"}>Most popular services in your area</h1>
+      <div className={"locations"}>
+        <ul className={"location-tags"}>
+          {locations.map((tag, index) => {
+            return (
+              <li key={tag}
+                  className={index === selectedTabIndex ? "selected" : "unselected"}
+                  onClick={() => setSelectedTabIndex(index)}>
+                {tag}
+                {index === selectedTabIndex && <div className={"selector"}/>}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      <div className={"skills-grid"}>
+        <ul className={"skills"}>
+          {
+            filteredSkills.map(({ location, skill }) => {
+              return (
+                <li key={`${location}-${skill}`}>
+                  <SkillCard location={location} skill={skill}/>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    </section>
+  )
+}
