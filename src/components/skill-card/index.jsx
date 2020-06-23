7026b5bff7408364e7e65d513dff1688fa288e7e
Edmond O'Flynn <oflynned@tcd.ie>
2020-06-23 13:32:16 +0200

    fix card text positioning on skills

--- src/components/skill-card/index.jsx
+++ src/components/skill-card/index.jsx
@@ -2,10 +2,14 @@ import React from "react"
 import "./style.scss"
 import { Card } from "../card"
 
+import "./style.scss"
+
 export const SkillCard = ({ skill, location }) => {
   return (
     <Card className={"skill-card"}>
-      {skill} agencies in {location}
+      {skill} agencies
+      <br/>
+      {location}
     </Card>
   )
 }
