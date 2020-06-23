657a0ae9ce2253cf8e6364a5acb8d8bbe8a41c90
Edmond O'Flynn <oflynned@tcd.ie>
2020-06-23 15:34:43 +0200

    add sections to index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,14 +5,19 @@ import "normalize.css"
 
 import Layout from "../components/layouts/layout"
 import SEO from "../components/layouts/seo"
+import Header from "../components/header"
 
-import "./index.scss"
 import { Hero } from "../components/sections/hero.section"
 import { Press } from "../components/sections/press.section"
-import Header from "../components/header"
 import { Footer } from "../components/footer"
 import { Process } from "../components/sections/process.section"
 import { PopularServices } from "../components/sections/popular-services.section"
+import { TrustPilot } from "../components/sections/trust-pilot.section"
+
+import "./index.scss"
+import { CoreValues } from "../components/sections/core-values.section"
+import { Divider } from "../components/divider"
+import { PopularAgenciesNearby } from "../components/sections/popular-agencies-nearby.section"
 
 const IndexPage = () => (
   <>
@@ -22,9 +27,21 @@ const IndexPage = () => (
       <Hero/>
       <PopularServices/>
       <Process/>
-
     </Layout>
+
+    <TrustPilot/>
+
+    <Layout>
+      <CoreValues/>
+    </Layout>
+
+
+    <Divider/>
     <Press/>
+    <Layout>
+      <PopularAgenciesNearby/>
+    </Layout>
+
     <Layout>
       <Footer/>
     </Layout>
