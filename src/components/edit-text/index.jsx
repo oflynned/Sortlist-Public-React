38f44164f2f49523edc4f31be3fc77408e5058fb
Edmond O'Flynn <oflynned@tcd.ie>
2020-06-23 18:55:59 +0200

    add aria label

--- src/components/edit-text/index.jsx
+++ src/components/edit-text/index.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import "./style.scss"
 
 export const EditText = ({ placeholder = "", styling = "" }) => {
-  return <input className={`edit-text ${styling}`} placeholder={placeholder}/>
+  return <input className={`edit-text ${styling}`} placeholder={placeholder} aria-label={"input"}/>
 }
 
 export const SearchBox = (props) => {
