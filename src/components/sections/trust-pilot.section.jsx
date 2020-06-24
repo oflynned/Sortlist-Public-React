76d058d6b32d25130853ff16b505d21b51e93b0b
Edmond O'Flynn <oflynned@tcd.ie>
2020-06-24 13:07:55 +0200

    add curved trust pilot section

--- src/components/sections/trust-pilot.section.jsx
+++ src/components/sections/trust-pilot.section.jsx
@@ -4,7 +4,10 @@ import "./trust-pilot.section.scss"
 export const TrustPilot = () => {
   return (
     <section className={"trust-pilot"}>
+      <div className={"curve"}/>
+      <div className={"half container"}>
 
+      </div>
     </section>
   )
 }
