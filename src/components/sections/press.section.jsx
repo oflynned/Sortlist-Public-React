07e7b2b31683d171823df6ce17f8702ad719d261
Edmond O'Flynn <oflynned@tcd.ie>
2020-06-24 12:59:54 +0200

    add press section

--- src/components/sections/press.section.jsx
+++ src/components/sections/press.section.jsx
@@ -2,10 +2,36 @@ import React from "react"
 
 import "./press.section.scss"
 
+import TechCrunch from "../../images/techcrunch.svg"
+
+const Journalist = () => {
+  return (
+    <div className={"image-container"}>
+      <img src={TechCrunch} alt={"tech crunch"}/>
+    </div>
+  )
+}
+
 export const Press = () => {
   return (
-    <div className={"press"}>
-
-    </div>
+    <section className={"full-width"}>
+      <div className={"press"}>
+          <h3 className={"fancy"}>Press</h3>
+        <div className={"journalists"}>
+          <Journalist/>
+          <Journalist/>
+          <Journalist/>
+          <Journalist/>
+          <Journalist/>
+          <Journalist/>
+          <Journalist/>
+          <Journalist/>
+          <Journalist/>
+          <Journalist/>
+          <Journalist/>
+          <Journalist/>
+        </div>
+      </div>
+    </section>
   )
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -39,7 +39,6 @@ const IndexPage = () => (
       <CoreValues/>
     </Layout>
 
-    <Divider/>
     <Press/>
 
     <Layout>
