4a5d6f9cfb77bf74c9612e28d3f85b436525222c
Edmond O'Flynn <oflynned@tcd.ie>
2020-06-25 12:17:23 +0200

    add main testimonial card

--- src/components/sections/trust-pilot.section.jsx
+++ src/components/sections/trust-pilot.section.jsx
@@ -1,12 +1,27 @@
 import React from "react"
-import "./trust-pilot.section.scss"
+
+import { PrimaryButton } from "../button"
+import { Card } from "../card"
+
 import Curve from "../../images/curve.svg"
 import TechCrunch from "../../images/techcrunch.svg"
-import { PrimaryButton } from "../button"
+import Lapresse from "../../images/lapresse.jpg"
+
+import "./trust-pilot.section.scss"
 
 const Company = () => (
   <div className={"company"}>
     <img src={TechCrunch} alt={"tech crunch"}/>
+  </div>
+)
+
+const Stars = () => (
+  <div className={"stars"}>
+    <span className={"material-icons"}>star</span>
+    <span className={"material-icons"}>star</span>
+    <span className={"material-icons"}>star</span>
+    <span className={"material-icons"}>star</span>
+    <span className={"material-icons"}>star</span>
   </div>
 )
 
@@ -18,13 +33,7 @@ export const TrustPilot = () => {
       </div>
       <div className={"container"}>
         <div className={"heading-container"}>
-          <div className={"stars"}>
-            <span className={"material-icons"}>star</span>
-            <span className={"material-icons"}>star</span>
-            <span className={"material-icons"}>star</span>
-            <span className={"material-icons"}>star</span>
-            <span className={"material-icons"}>star</span>
-          </div>
+          <Stars/>
           <h3 className={"centred fancy"}>Thousands of companies</h3>
           <h3 className={"centred fancy"}>found their ideal agency via Sortlist</h3>
         </div>
@@ -45,7 +54,28 @@ export const TrustPilot = () => {
           <Company/>
           <Company/>
         </div>
+
+        <Card className={"main-testimonial"} shadow>
+          <div className={"testimonial-container"}>
+            <div className={"text-container"}>
+              <Stars/>
+              <h1 className={"fancy"}>&quot;To summarize, Sortlist in three words I would say: ease, proximity, useful.&quot;</h1>
+              <div className={"role"}>
+                <h3>Tatiana Debrabandere</h3>
+                <p>Project Manager at Lapresse</p>
+              </div>
+              <div className={"read-more"}>
+                <a>Read more about Tatiana Debrabandere's experience</a>
+                <span className={"material-icons"}>arrow_forward</span>
+              </div>
+            </div>
+            <div className={"image-container"}>
+              <img src={Lapresse} alt={"testimonial"}/>
+            </div>
+          </div>
+        </Card>
       </div>
+
 
       <div className={"reviews-container"}>
         <PrimaryButton>Find my agency</PrimaryButton>
